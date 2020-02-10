4d957c7b815c63c98a541b00ef55530576825c19
Alexander Rubenov <a.a.rubenov@tinkoff.ru>
2020-02-10 19:57:25 +0300

    done lesson 45

--- src/components/add-task/add-task.js
+++ src/components/add-task/add-task.js
@@ -2,19 +2,40 @@ import React, { Component } from 'react'
 import './add-task.css'
 
 export default class AddTask extends Component {
+  state = {
+    title: '',
+  }
+
+  onInputChange = event => {
+    this.setState({
+      title: event.target.value,
+    })
+  }
+
+  onFormSubmit = event => {
+    event.preventDefault()
+    this.props.addTask(this.state.title)
+    this.setState({
+      title: '',
+    })
+  }
+
   render() {
-    const { addTask } = this.props
+    const { title } = this.state
 
     return (
-      <div className="add-task-form">
-        <button
-          className="btn btn-outline-secondary"
-          type="button"
-          onClick={() => addTask('Тестовый текст')}
-        >
+      <form className="add-task-form d-flex" onSubmit={this.onFormSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onInputChange}
+          placeholder="Добавь новую задачу"
+          value={title}
+        />
+        <button className="btn btn-outline-secondary" type="submit">
           Добавить задачу
         </button>
-      </div>
+      </form>
     )
   }
 }
